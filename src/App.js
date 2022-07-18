4c81c2e2a53d2649f139e5cfd681f447d07e986d
ahmedebid <ahmed-ebid@outlook.com>
2022-07-18 20:57:18 +0200

    Add game's name and instructions on how to play it

--- src/App.js
+++ src/App.js
@@ -43,6 +43,10 @@ export default function App() {
 
     return (
         <main>
+            <h1 className="game-title">Tenzies</h1>
+            <p className="game-instructions">
+                Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+            </p>
             <div className="dice">
                 {dieElements}
             </div>
